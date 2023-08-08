0d32e33f025dcac01b6c3e8a6498870191fb4ac6
VK <370725567@qq.com>
2023-08-08 10:38:29 +0800

    修改注释

--- uniCloud-aliyun/cloudfunctions/vkspay-test/index.obj.js
+++ uniCloud-aliyun/cloudfunctions/vkspay-test/index.obj.js
@@ -1,8 +1,7 @@
-// 云对象教程: https://uniapp.dcloud.net.cn/uniCloud/cloud-obj
-// jsdoc语法提示教程：https://ask.dcloud.net.cn/docs/#//ask.dcloud.net.cn/article/129
+// 引入工具类
 const payUtil = require("./payUtil");
 
-// 配置
+// 引入配置
 const config = require("./config");
 
 const db = uniCloud.database();
